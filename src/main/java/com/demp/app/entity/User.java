8006cd1c879b44c890b44148330bf392505e2fb8
prakash8876 <42605640+prakash8876@users.noreply.github.com>
2022-04-05 16:01:01 +0530

    changed generatedvalue

--- src/main/java/com/demp/app/entity/User.java
+++ src/main/java/com/demp/app/entity/User.java
@@ -3,6 +3,7 @@ package com.demp.app.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -18,7 +19,7 @@ import lombok.NoArgsConstructor;
 public class User {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
 	private int id;
 	
